75737d68058450c92d1ac624684ff589acaf633c
Jacob Wahlgren <jacobwah@kth.se>
2021-12-10 23:13:58 +0100

    Parallel build

--- build_tegner.sh
+++ build_tegner.sh
@@ -2,7 +2,9 @@
 
 module load cuda/10.1
 
-make CFLAGS="-std=c++11 -arch=sm_30" outdir=sm30
-make CFLAGS="-std=c++11 -arch=sm_30 -DPINNED" outdir=sm30p
-make CFLAGS="-std=c++11 -arch=sm_37" outdir=sm37
-make CFLAGS="-std=c++11 -arch=sm_37 -DPINNED" outdir=sm37p
+make CFLAGS="-std=c++11 -arch=sm_30" outdir=sm30 &
+make CFLAGS="-std=c++11 -arch=sm_30 -DPINNED" outdir=sm30p &
+make CFLAGS="-std=c++11 -arch=sm_37" outdir=sm37 &
+make CFLAGS="-std=c++11 -arch=sm_37 -DPINNED" outdir=sm37p &
+
+wait
